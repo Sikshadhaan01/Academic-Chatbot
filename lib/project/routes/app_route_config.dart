390ef7b79d13303677e676d9a2d7c10751ac0244
aswathy236 <aswathyv236@gmail.com>
2024-04-12 13:39:59 +0530

    updated

---
+++ lib/project/routes/app_route_config.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_new/pages/chat_page.dart';
+import 'package:flutter_new/pages/home_page.dart';
+import 'package:flutter_new/pages/login_page.dart';
+import 'package:flutter_new/pages/signup_page.dart';
+import 'package:flutter_new/pages/teacher_page.dart';
+import 'package:flutter_new/pages/topics_page.dart';
+import 'package:flutter_new/project/routes/app_route_constants.dart';
+import 'package:go_router/go_router.dart';
+
+class MyAppRouter{
+GoRouter router=GoRouter(
+  routes: [
+    GoRoute(
+      name:MyAppRouteConstant.login_pageRouteName,
+      path: '/',
+      pageBuilder: (context, state) {
+        return MaterialPage(child:LoginPage());
+      },
+    ),
+    GoRoute(
+      name: MyAppRouteConstant.home_pageRouteName,
+      path: '/home_page',
+      pageBuilder: (context, state) {
+        return MaterialPage(child:HomePage());
+      },
+    ),
+    GoRoute(
+      name: MyAppRouteConstant.signup_pageRouteName,
+      path: '/signup_page',
+      pageBuilder: (context, state) {
+        return MaterialPage(child:SignupPage());
+      },
+    ),
+    GoRoute(
+      name: MyAppRouteConstant.teacher_pageRouteName,
+      path: '/teacher_page',
+      pageBuilder: (context, state) {
+        return MaterialPage(child:TeacherPage());
+      },
+    ),
+    GoRoute(
+      name: MyAppRouteConstant.topics_pageRouteName,
+      path: '/topics_page',
+      pageBuilder: (context, state) {
+        return MaterialPage(child:TopicsPage());
+      },
+    ),
+    GoRoute(
+      name: MyAppRouteConstant.chat_pageRouteName,
+      path: '/chat_page',
+      pageBuilder: (context, state) {
+        return MaterialPage(child:ChatPage());
+      },
+    ),
+    GoRoute(
+      name: MyAppRouteConstant.fpassword_pageRouteName,
+      path: '/fpassword_page',
+      pageBuilder: (context, state) {
+        return MaterialPage(child:ChatPage());
+      },
+    )
+  ]
+);
+}
---
+++ lib/project/routes/app_route_constants.dart
@@ -0,0 +1,12 @@
+class MyAppRouteConstant{
+
+static const String home_pageRouteName='home_page';
+static const String chat_pageRouteName='chat_page';
+static const String fpassword_pageRouteName='password_page';
+static const String login_pageRouteName='login_page';
+static const String teacher_pageRouteName='teacher_page';
+static const String topics_pageRouteName='topics_page';
+static const String signup_pageRouteName='signup_page';
+
+
+}
